e426df05fd50cf9526dd76d59bc1605a58573b81
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2024-02-23 10:49:45 +0600

    update apis

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -5,12 +5,25 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://pokeapi.co/api/v2/" }),
+  tagTypes: ["Pokemon"],
   endpoints: (builder) => ({
     getPokemonByName: builder.query({
       query: (name) => ({
         url: `pokemon/${name}`,
         method: "GET",
       }),
+      providesTags: ["Pokemon"],
+    }),
+    addPokemonByName: builder.mutation({
+      query: (data) => {
+        console.log(data);
+        return {
+          url: `pokemon/${data.id}`,
+          method: "POST",
+          body: data?.pokemon,
+        };
+      },
+      invalidatesTags: ["Pokemon"],
     }),
   }),
 });
